a2b90f2715f8b6e111eb8bf141c20621d984a7df
Dennis Dinwiddie <dennisdinwiddie@gmail.com>
2026-01-29 17:39:42 -0500

    fix build on mac hopefully

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname "$0")"
 ZIP_NAME="fixhub.zip"
+FILES="manifest.json popup.html popup.js content.js styles.css fh-icon.png"
 
 rm -f "$ZIP_NAME"
-7z a "$ZIP_NAME" manifest.json popup.html popup.js content.js styles.css fh-icon.png
+
+python3 -c "
+import zipfile, sys
+with zipfile.ZipFile('$ZIP_NAME', 'w', zipfile.ZIP_DEFLATED) as zf:
+    for f in '$FILES'.split():
+        zf.write(f)
+"
 
 echo "Created $ZIP_NAME"
